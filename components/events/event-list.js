32bdb285d546c96b2db3f5ecfedcdbb8ece01b14
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-12-06 06:28:22 +0000

    event page, all events page

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -3,12 +3,13 @@ import classes from './event-list.module.css';
 
 const EventList = (props) => {
   const { items } = props;
-  console.log(props);
+  console.log(props.items);
 
   return (
     <ul className={classes.list}>
       {items.map((event) => (
         <EventItem
+          key={event.id}
           id={event.id}
           title={event.title}
           location={event.location}
